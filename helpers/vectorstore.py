624afb2a3b6adaf25fafd438d804f0f4a151026e
Qadeer Ahmad <82707533+mrqadeer@users.noreply.github.com>
2024-10-16 11:12:50 +0500

    Update vectorstore.py

--- helpers/vectorstore.py
+++ helpers/vectorstore.py
@@ -1,5 +1,5 @@
 
-from langchain.embeddings import HuggingFaceEmbeddings,OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 import streamlit as st 
 
@@ -18,23 +18,11 @@ class VectorStore:
         # creating the Vectore Store using Facebook AI Semantic search
         
         file_name=helper.get_file_name(files)
-        
-        
-        if os.path.exists(f"embeddings/{file_name}.pkl"):
-            try:
-                with open(f"embeddings/{file_name}.pkl", "rb") as rf:
-                    knowledge_base = pickle.load(rf)
-            except FileNotFoundError as fe:
-                st.warning(f"File not found")
-            else:
-                st.info("Vector stored loaded successfully.")
-        else:
-            try:
-                # embeddings = OpenAIEmbeddings(api_key=api_key)
-                embeddings=HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
-                knowledge_base = FAISS.from_texts(text_chunks,embeddings)
-                with open(f"embeddings/{file_name}.pkl", "wb") as wf:
-                    pickle.dump(knowledge_base, wf)
+        try:
+            embeddings = OpenAIEmbeddings(api_key=api_key)
+            #embeddings=HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
+            knowledge_base = FAISS.from_texts(text_chunks,embeddings)
+                
 
             except EOFError as e:
                 st.error("Issue with your file.")
